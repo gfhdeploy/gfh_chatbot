84a808571afad242a1fac93a887d9fe143ca00a0
soufiane001 <soufiane.hayou@gmail.com>
2024-08-18 17:21:34 -0700

    update

--- main.py
+++ main.py
@@ -48,15 +48,15 @@ for message in st.session_state.chat_history:
 user_prompt = st.chat_input("Ask a question..")
 
 
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            div.embeddedAppMetaInfoBar_container__DxxL1 {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+# hide_st_style = """
+#             <style>
+#             #MainMenu {visibility: hidden;}
+#             footer {visibility: hidden;}
+#             header {visibility: hidden;}
+#             div.embeddedAppMetaInfoBar_container__DxxL1 {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 if user_prompt:
